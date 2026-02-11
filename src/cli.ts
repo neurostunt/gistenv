e702e92597e4f470f33f6337395c056b99e3764b
Neurostunt <neurostunt@gmail.com>
2026-02-11 12:28:16 +0100

    chore: bump version to 0.1.10

--- src/cli.ts
+++ src/cli.ts
@@ -66,7 +66,7 @@ const DEFAULT_UPLOAD_FILE = '.env';
 program
   .name('gistenv')
   .description('Upload .env to Gist as a section, or download a section into .env')
-  .version('0.1.9');
+  .version('0.1.10');
 
 defineSectionsCommand();
 defineListCommand();
